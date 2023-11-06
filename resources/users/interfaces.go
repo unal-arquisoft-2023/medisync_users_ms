4285b1f61dd527687851384f64d66ff7daeb2920
sechmo <sesanchezmo@unal.edu.co>
2023-11-06 02:35:50 -0500

    A little documentation for the pat repo interface

--- resources/users/interfaces.go
+++ resources/users/interfaces.go
@@ -31,9 +31,14 @@ type PatientUpdateInput struct {
 }
 
 type PatientRepository interface {
+	// FindOne returns a patient given its id
 	FindOne(ctx context.Context, id string) (*domain.Patient, UserRepositoryError)
+	// Create creates a new patient and returns it
 	Create(ctx context.Context, input PatientCreationInput) (*domain.Patient, UserRepositoryError)
+	// Update updates a patient and returns the updated object
 	Update(ctx context.Context, input PatientUpdateInput) (*domain.Patient, UserRepositoryError)
+	// Suspends a patient and returns the updated object
 	Suspend(ctx context.Context, id string) (*domain.Patient, UserRepositoryError)
+	// Activates a patient and returns the updated object
 	Activate(ctx context.Context, id string) (*domain.Patient, UserRepositoryError)
 }
