4a2826678434b945f6d6cf05da12ef20a30456a4
sechmo <sesanchezmo@unal.edu.co>
2023-11-06 12:51:06 -0500

    Extracted generic binder for general use in routes

---
+++ routes/common.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func bindJSON[T any](f func(c echo.Context, req T) error) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		var req T
+		if err := c.Bind(&req); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+
+		return f(c, req)
+	}
+}
--- routes/patient_route.go
+++ routes/patient_route.go
@@ -2,24 +2,11 @@ package routes
 
 import (
 	"medysinc_user_ms/controllers/patient"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func bind[T any](f func(c echo.Context, req T) error) echo.HandlerFunc {
-	return func(c echo.Context) error {
-
-		var req T
-		if err := c.Bind(&req); err != nil {
-			return c.JSON(http.StatusBadRequest, err.Error())
-		}
-
-		return f(c, req)
-	}
-}
-
 func PatientRoute(e *echo.Echo, patCon *patient.PatientController) {
 	//pacientes
-	e.POST("/patient", bind[patient.CreatePatientRequest](patCon.CreatePatient))
+	e.POST("/patient", bindJSON[patient.CreatePatientRequest](patCon.CreatePatient))
 }
